ccbd8297e4f4ce8898ccfc9efb36c93176590ef1
konrad-py <konradd.py@gmail.com>
2026-06-17 20:16:19 +0200

    dodanie produktow do sqlite

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from flask import Flask, render_template, request, redirect, url_for, g
-import json, sqlite3
+import sqlite3
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
@@ -16,6 +16,7 @@ def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DB_PATH)
+        db.row_factory = sqlite3.Row
     return db
 
 @app.teardown_appcontext
@@ -42,9 +43,14 @@ with app.app_context():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+
     edit_id = request.args.get('edit')
     if request.method == "GET":
 
+        db = get_db()
+
+        cursor = db.execute('SELECT id, produkt, ilosc FROM produkty')
+        produkty = cursor.fetchall()
         
         if edit_id is not None:
             produkt_value = produkt[int(edit_id)][0]
@@ -56,16 +62,17 @@ def index():
             form_action = url_for('index')
 
         
-        return render_template("index.html", produkt=produkt, produkt_value=produkt_value, ilosc_value=ilosc_value, form_action=form_action)
+        return render_template("index.html", produkty=produkty, produkt_value=produkt_value, ilosc_value=ilosc_value, form_action=form_action)
     
     else:
+        db = get_db()
         if edit_id is not None:
             produkt[int(edit_id)] = (request.form['produkt'], request.form['ilosc'])
         else:
-            produkt.append((request.form["produkt"], request.form["ilosc"]))
+            db.execute('INSERT INTO produkty (produkt, ilosc) VALUES (?, ?)', (request.form['produkt'], request.form['ilosc']))
+            db.commit()
         
-        with open('./data.json', 'w') as f:
-            json.dump(produkt, f)
+        
 
         return redirect(url_for('index'))
 
